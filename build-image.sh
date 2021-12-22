74bb86651c7b374a91bddc67106280ebcbd585c1
devopstales <42894256+devopstales@users.noreply.github.com>
2021-12-22 15:54:08 +0100

    2.2

--- build-image.sh
+++ build-image.sh
@@ -9,8 +9,8 @@ if [ ! -f /tmp/trivy ]; then
 fi
 cp /tmp/trivy docker
 cp trivy-operator.py docker/trivy-operator.py
-kim build --tag ${1}-devel docker
-#docker build -t ${1} docker
+#kim build --tag ${1}-devel docker
+docker build -t ${1} docker
 rm -f docker/trivy-operator.py
 rm -f docker/trivy
-#docker push $1
+docker push $1
