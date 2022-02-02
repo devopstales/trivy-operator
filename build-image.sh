2624319a3e7cbe551e65f43ada86faee786fec05
devopstales <42894256+devopstales@users.noreply.github.com>
2022-02-02 16:56:53 +0100

    add multi arch

--- build-image.sh
+++ build-image.sh
@@ -9,8 +9,27 @@ if [ ! -f /tmp/trivy ]; then
 fi
 cp /tmp/trivy docker
 cp trivy-operator.py docker/trivy-operator.py
+#############################################################
+# Docker build
+#############################################################
 #kim build --tag ${1}-devel docker
 docker build -t ${1} docker
-rm -f docker/trivy-operator.py
-rm -f docker/trivy
-docker push $1
+#docker build -t ${1}-arm32v7 --build-arg ARCH=arm32v7/ docker
+#docker build -t ${1}-arm64v8 --build-arg ARCH=arm64v8/ docker
+#############################################################
+# Docker push
+#############################################################
+#docker push $1
+#docker push ${1}-arm32v7
+#docker push ${1}-arm64v8
+#docker manifest create \
+#devopstales/trivy-operator:latest \
+#--amend ${1} \
+#--amend ${1}-arm32v7 \
+#--amend ${1}-arm64v8
+#docker manifest push devopstales/trivy-operator:latest
+#############################################################
+# Docker Cleanup
+#############################################################
+#rm -f docker/trivy-operator.py
+#rm -f docker/trivy
