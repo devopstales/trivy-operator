f3623c87a87c8a08c0520f4ffb9fd6f8bb613c65
devopstales <42894256+devopstales@users.noreply.github.com>
2022-07-11 12:22:28 +0200

    fix: getpwuid(): uid not found: 1001

--- docker/trivy-operator/entrypoint.sh
+++ docker/trivy-operator/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Fix getpwuid(): uid not found: 1001 by setting the USER env var to prevent python from looking for a matching uid/gid in the password database.
+# See https://github.com/python/cpython/blob/v3.6.0/Lib/getpass.py#L155-L170.
+USER=$(id -u)
+echo "Setting USER environment variable to ${USER}"
+export USER=$USER
+
 if [[ $LOG_LEVEL == "DEBUG" ]];
 then
 	kopf run -v -A /trivy-operator.py --log-format=full
