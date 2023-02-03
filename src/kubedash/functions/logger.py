dcdedf4579fee50f7a297c747f5ed34e67b3425a
devopstales <42894256+devopstales@users.noreply.github.com>
2023-02-03 18:41:02 +0100

    feat!: moce kubedash to separate repo

--- src/kubedash/functions/logger.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import logging
-
-FORMAT = '[%(asctime)s] %(name)s        %(levelname)s %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.INFO)
-logger = logging.getLogger('KubeDash')
